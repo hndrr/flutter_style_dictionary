b4c9dd538641dad09922c6ebf61e1eed5025af65
hndrr <yozhik20@gmail.com>
2022-12-02 01:32:08 +0900

    commit

---
+++ style-dictionary/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Thu, 01 Dec 2022 16:12:25 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
